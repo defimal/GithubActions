4beab3523c1db4b5d2a110aa5393122d0e0daf41
Defi <89351972+defimal@users.noreply.github.com>
2026-02-28 00:18:08 -0600

    Implemented Power method

--- Console/Program.cs
+++ Console/Program.cs
@@ -84,8 +84,8 @@ public class Program
 	}
 
 	// Implement this method following a similar pattern as above
-	public static double Power(string x, string y)
+	public static double Power(double baseNum, int exponent)
 	{
-		return 0.0;
+		return Math.Pow(baseNum, exponent);
 	}
 }
